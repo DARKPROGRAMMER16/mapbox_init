72ec77184a7cd10dc195244b0fdeb477d565b962
Rajat Dash <rajat@Rajats-MacBook-Air.local>
2022-01-24 13:35:24 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Mbpx from './components/Mbpx';
+import './App.css';
+
+const App = () => {
+  return (
+    <Fragment>
+      <Mbpx/>
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ src/components/Mbpx.jsx
@@ -0,0 +1,45 @@
+import React, { Fragment,useRef, useEffect,useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGFyazE2IiwiYSI6ImNreXNjeW5peDBsZm4ycXMwMndheG4zeHIifQ.izA6atCU_0mQxQmtlAvcMg';
+
+const Mbpx = () => {
+
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        });
+    });
+
+    useEffect(() => {
+        if (!map.current) return; // wait for map to initialize
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+        });
+    });
+
+  return (
+      <Fragment>
+            <div>
+                <div className="sidebar">
+                    Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+                </div>
+                <div ref={mapContainer} className="map-container" />
+            </div>
+      </Fragment>
+  );
+};
+
+export default Mbpx;
